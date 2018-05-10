8ddd6300fde9d7eb3cfe5f86448a978c5705cc14
Nico-Rode <nrode17@my.whitworth.edu>
2018-05-10 14:41:15 -0700

    added email service to retrieve db file

--- Website HTML and PHP files/retrieve_dbData.php
+++ Website HTML and PHP files/retrieve_dbData.php
@@ -98,14 +98,45 @@ else {
      "<br>";
 
   }
-    print("\n");
-    $p1->printNewperson();
-    $compare1->printPerson();
-    $compare2->printPerson();
-    $compare3->printPerson();
-  $link->close();
+
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  use PHPMailer\PHPMailer\STMP;
+
+  require 'libs/phpmailer/src/Exception.php';
+  require 'libs/phpmailer/src/PHPMailer.php';
+  require 'libs/phpmailer/src/SMTP.php';
+
+  $mail = new PHPMailer(); // create a new object
+  $mail->IsSMTP(); // enable SMTP
+  $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+  $mail->SMTPAuth = true; // authentication enabled
+  $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+  $mail->Host = "smtp.gmail.com";
+  $mail->Port = 465; // or 587
+  $mail->IsHTML(true);
+  $mail->Username = "9621312@gmail.com";
+  $mail->Password = "2BlueTankJumped!";
+  $mail->SetFrom("9621312@gmail.com");
+  $mail->Subject = "Test";
+  $mail->Body = "hello";
+  $mail->AddAddress("nico@rodester.com");
+
+  $age = $compare1->getAge() . "\r\n";
+  $cancerType = $compare1->getCancerType();
+  $roleToCancer = $compare1->getRole();
+  $treatmentLocation = $compare1->getTreatementLoctation();
 
 
 
+  $mail->Body = "Name : John Doe " . PHP_EOL .
+  "Age : {$age} \r\n" . PHP_EOL .
+  "Cancer Type : {$cancerType} \r\n" . PHP_EOL .
+  "Role to Cancer : {$roleToCancer}";
 
- ?>
+   if(!$mail->Send()) {
+      echo "Mailer Error: " . $mail->ErrorInfo;
+   } else {
+      echo "Message has been sent";
+   }
+  ?>
